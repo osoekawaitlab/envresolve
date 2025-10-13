bb0ec942dce6f8a5d152b6fa9f14a2dca5f24c52
osoken <osoken@osoekawalab.com>
2025-10-13 23:55:45 +0900

    chore: bump version to 0.1.1

--- src/envresolve/__init__.py
+++ src/envresolve/__init__.py
@@ -10,7 +10,7 @@ from envresolve.exceptions import (
 )
 from envresolve.services.expansion import expand_variables
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "CircularReferenceError",
